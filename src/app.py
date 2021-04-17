ea7b4133362d0ad6b996218a238f9b8dded28119
mini-hiori <phantombisco@gmail.com>
2021-04-17 17:48:43 +0900

    基礎組み完成

---
+++ src/app.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+from mangum import Mangum
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class HelloParam(BaseModel):
+    name: str
+
+@app.get("/hello")
+def get_hello(name: str=None):
+    """
+    getでおへんじする
+    """
+    if name:
+        message = f"[GET]hello, {name}!"
+    else:
+        message = f"[GET]hello, visitor!"
+
+    return {"message": message}
+
+@app.post("/hello_post")
+def post_hello(param: HelloParam):
+    """
+    postでおへんじする
+    """
+    if not param.name:
+        raise HTTPException(
+            status_code=400,
+            detail="おなまえがないよ"
+        )
+    else:
+        message = f"[GET]hello, {name}!"
+
+    return {"message": message}
+
+
+handler = Mangum(app, False)
